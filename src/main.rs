f4805487990f172d0724fe932256a1152d1088c4
pigeon <6261rhys@gmail.com>
2023-10-12 16:47:07 +0900

    Add api response fetcher

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use structopt::StructOpt;
+use exitfailure::{ExitFailure};
+use serde_derive::{Deserialize, Serialize};
+use reqwest::Url;
 
 #[derive(StructOpt)]
 struct Cli {
     word: String
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Thesaurus {
     id: String,
     uuid: String,
@@ -16,7 +20,19 @@ struct Thesaurus {
     offensive: bool
 }
 
-fn main() {
+impl Thesaurus {
+    async fn get(word: &String) -> Result<Self, ExitFailure> {
+        let url = format!("https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{}?key=54fc6c18-554f-4c36-8b35-ef1835487d81", word);
+        let url = Url::parse(&*url)?;
+        let resp =reqwest::get(url).await?.json::<Thesaurus>().await?;
+        Ok(resp)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
-    println!("Input: {}", args.word)
+    let resp = Thesaurus::get(&args.word).await?;
+    println!("{}: {}", resp.section,args.word);
+    Ok(())
 }
