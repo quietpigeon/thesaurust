c9faf4fd202e0047a93c74dfabe1ad4cc769765d
pigeon <6261rhys@gmail.com>
2023-10-12 20:39:18 +0900

    Fix json serialization

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,74 @@
-use structopt::StructOpt;
-use exitfailure::{ExitFailure};
-use serde_derive::{Deserialize, Serialize};
+use exitfailure::ExitFailure;
 use reqwest::Url;
+use serde_derive::{Deserialize, Serialize};
+use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Cli {
-    word: String
+    word: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Thesaurus {
-    id: String,
-    uuid: String,
-    src: String,
-    section: String,
-    target: Vec<String>,
-    syns: Vec<String>,
-    ants: Vec<String>,
-    offensive: bool
+    word: String,
+    origin: Option<String>,
+    meanings: Vec<Meaning>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Meaning {
+    partOfSpeech: String,
+    definitions: Vec<Definition>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Definition {
+    definition: String,
+    example: Option<String>,
+    synonyms: Vec<String>,
+    antonyms: Vec<String>,
+}
+
+/** 
 impl Thesaurus {
     async fn get(word: &String) -> Result<Self, ExitFailure> {
-        let url = format!("https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{}?key=54fc6c18-554f-4c36-8b35-ef1835487d81", word);
+        let url = format!("https://api.dictionaryapi.dev/api/v2/entries/en/{}", word);
         let url = Url::parse(&*url)?;
-        let resp =reqwest::get(url).await?.json::<Thesaurus>().await?;
-        Ok(resp)
+        let resp = reqwest::get(url)
+            .await?
+            .json::<Vec<Thesaurus>>()
+            .await?;
+        let r = &resp[0];
+        Ok(r)
     }
 }
+*/
 
 #[tokio::main]
 async fn main() -> Result<(), ExitFailure> {
+    let data = r#"[{
+            "word": "blue",
+            "origin": "origin",
+            "meanings": [{
+                "partOfSpeech": "wee",
+                "definitions": [{
+                    "definition": "",
+                    "example": "",
+                    "synonyms": [""],
+                    "antonyms": [""]
+                }]
+            }]
+    }]"#;
     let args = Cli::from_args();
-    let resp = Thesaurus::get(&args.word).await?;
-    println!("{}: {}", resp.section,args.word);
+    let url = format!("https://api.dictionaryapi.dev/api/v2/entries/en/{}", args.word);
+    println!("{}", url);
+    let response = reqwest::get(&url).await?;
+    let r: Vec<Thesaurus> = response.json().await?;
+    //let resp = Thesaurus::get(&args.word).await?;
+    let datas: Vec<Thesaurus> = serde_json::from_str(data)?;
+    println!("{:?}", r);
+    for data in datas.iter() {
+        println!("{:#?}", data.origin);
+}
     Ok(())
 }
