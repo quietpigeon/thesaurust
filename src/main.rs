7ead15da8a635e7d7ea064c737052ea5ad27c2d3
moreen h <6261rhys@gmail.com>
2023-10-13 16:27:55 +0900

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ async fn main() -> Result<(), ExitFailure> {
     let url = format!("https://api.dictionaryapi.dev/api/v2/entries/en/{}", args.word);
     let response = reqwest::get(&url).await?;
     let results: Vec<protocol::Thesaurus > = response.json().await?;
+    // There can be more than one results from the API response, but only the first element is chosen for simplicity. 
     let result = &results[0];
     println!("{}", result.meanings[0].partOfSpeech);
     println!("{}", result.meanings[0].definitions[0].definition);
--- src/protocol/data.rs
+++ src/protocol/data.rs
@@ -1,15 +1,19 @@
 use serde_derive::{ Deserialize, Serialize };
 use structopt::StructOpt;
 
+/// The word that the user wants to search for in the thesaurus.
 #[derive(StructOpt)]
 pub struct Cli {
     pub word: String,
 }
 
+/// Components of a response from the Free Dictionary API.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Thesaurus {
     pub word: String,
     pub origin: Option<String>,
+
+    // A word can have multiple meanings, hence it is represented as an array of meanings.
     pub meanings: Vec<Meaning>
 }
 
