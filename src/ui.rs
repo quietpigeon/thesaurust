bd77eff65082b89649b26329497efe8a27ae7fad
pigeon <6261rhys@gmail.com>
2023-10-22 18:01:05 +0900

    Change layout. Add example and similar blocks.

--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,8 @@
 use ratatui::{
     layout::{Direction, Layout},
     prelude::Constraint,
-    style::{Color, Style},
-    widgets::{Block, Borders, Paragraph, Wrap},
+    style::{Color, Modifier, Style, Stylize},
+    widgets::{Block, Borders, Paragraph, Wrap, Padding},
 };
 
 use crate::{
@@ -19,7 +19,7 @@ pub fn render(app: &mut App, f: &mut Frame) {
         .constraints(
             [
                 Constraint::Length(3),
-                Constraint::Length(9),
+                Constraint::Length(12),
                 Constraint::Min(1),
             ]
             .as_ref(),
@@ -35,17 +35,30 @@ pub fn render(app: &mut App, f: &mut Frame) {
     // The `lower_frame` consists of the `part_of_speech` block and the `definitions` block.
     let lower_frame = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(25), Constraint::Percentage(75)].as_ref())
+        .constraints([Constraint::Percentage(10), Constraint::Percentage(80), Constraint::Percentage(10)].as_ref())
         .margin(1)
         .split(main_frame[1]);
 
+    let right_frame = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Percentage(50),
+                Constraint::Percentage(50),
+            ]
+            .as_ref(),
+        )
+        .split(lower_frame[1]);
+
     let mut part_of_speech = String::from("");
     let mut definition = String::from("");
+    let mut example = String::from("");
     if !app.results.is_empty() {
         part_of_speech = Thesaurus::unwrap_meanings_at(0, &app.results[0]).0;
         let definitions = Thesaurus::unwrap_meanings_at(0, &app.results[0]).1;
         if definitions.len() > 0 {
             definition = definitions[0].definition.as_ref().unwrap().to_string();
+            example = definitions[0].example.as_ref().unwrap().to_string();
         }
     }
 
@@ -98,8 +111,26 @@ pub fn render(app: &mut App, f: &mut Frame) {
             .style(Style::default().fg(Color::Green))
             .wrap(Wrap { trim: true })
             .block(Block::default().borders(Borders::ALL).title("Definition")),
-        lower_frame[1],
+        right_frame[0],
     );
+
+    // Example block.
+    f.render_widget(
+        Paragraph::new(String::from(example).add_modifier(Modifier::ITALIC))
+            .style(Style::default())
+            .wrap(Wrap { trim: true })
+            .block(Block::default().borders(Borders::ALL).title("Example")),
+        right_frame[1],
+    );
+
+    // Synonym block.
+    f.render_widget(
+        Paragraph::new(String::from(""))
+            .style(Style::default())
+            .wrap(Wrap { trim: true })
+            .block(Block::default().borders(Borders::ALL).title("Similar")),
+        lower_frame[2],
+    )
 
     // TODO: Add synonyms and antonyms block.
 }
