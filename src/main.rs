7d1616c081747ef1d8b11cfda1d0d0940592954d
pigeon <6261rhys@gmail.com>
2023-11-03 15:18:03 +0900

    Add keybindings for exiting the app

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod app;
+mod banner;
 mod client;
 mod data;
 mod errors;
@@ -6,15 +7,14 @@ mod list;
 mod selection;
 mod tui;
 mod ui;
-mod banner;
 
 use anyhow::Result;
-use app::{ App, InputMode };
+use app::{App, InputMode};
 use client::get_data;
-use crossterm::event::{ self, Event, KeyCode };
-use ratatui::{ backend::CrosstermBackend, Terminal };
+use crossterm::event::{self, Event, KeyCode};
+use ratatui::{backend::CrosstermBackend, Terminal};
 use tui::Tui;
-use tui_input::backend::crossterm::EventHandler;
+use tui_input::{backend::crossterm::EventHandler, Input};
 
 fn main() -> Result<()> {
     let mut app = App::new();
@@ -28,54 +28,48 @@ fn main() -> Result<()> {
         tui.draw(&mut app)?;
         if let Event::Key(key) = event::read()? {
             match app.input_mode {
-                InputMode::Normal =>
-                    match key.code {
-                        KeyCode::Esc => {
-                            App::quit(&mut app);
-                        }
-                        KeyCode::Char(':') | KeyCode::Char('j') | KeyCode::Char('e') => {
-                            app.input_mode = InputMode::Selecting;
-                        }
-                        KeyCode::Char('/') => {
-                            app.input_mode = InputMode::Editing;
-                            app.input.reset();
-                        }
-                        _ => {}
+                InputMode::Normal => match key.code {
+                    KeyCode::Char('q') | KeyCode::Esc => {
+                        App::quit(&mut app);
                     }
-                InputMode::Editing =>
-                    match key.code {
-                        KeyCode::Enter => {
-                            app.input_mode = InputMode::Normal;
+                    KeyCode::Char(':') | KeyCode::Char('j') | KeyCode::Char('e') => {
+                        app.input_mode = InputMode::Selecting;
+                    }
+                    KeyCode::Char('/') => {
+                        app.input_mode = InputMode::Editing;
+                        app.input.reset();
+                    }
+                    _ => {}
+                },
+                InputMode::Editing => match key.code {
+                    KeyCode::Enter => {
+                        app.input_mode = InputMode::Normal;
 
-                            // Fetch data
-                            app.results = get_data(app.input.to_string());
+                        // Fetch data
+                        app.results = get_data(app.input.to_string());
 
-                            // Propagate the data into the corresponding stateful lists.
-                            App::update_selections(&mut app);
-                        }
-                        KeyCode::Esc => {
-                            app.input_mode = InputMode::Normal;
-                        }
-                        _ => {
-                            app.input.handle_event(&Event::Key(key));
-                        }
+                        // Propagate the data into the corresponding stateful lists.
+                        App::update_selections(&mut app);
                     }
-                InputMode::Selecting =>
-                    match key.code {
-                        KeyCode::Char('j') => {
-                            app.selections.down();
-                        }
-                        KeyCode::Char('k') => {
-                            app.selections.up();
-                        }
-                        KeyCode::Char('q') => {
-                            app.input_mode = InputMode::Normal;
-                        }
-                        KeyCode::Esc => {
-                            App::quit(&mut app)
-                        }
-                        _ => {}
+                    KeyCode::Esc => {
+                        app.input_mode = InputMode::Normal;
                     }
+                    _ => {
+                        app.input.handle_event(&Event::Key(key));
+                    }
+                },
+                InputMode::Selecting => match key.code {
+                    KeyCode::Char('j') => {
+                        app.selections.down();
+                    }
+                    KeyCode::Char('k') => {
+                        app.selections.up();
+                    }
+                    KeyCode::Char('q') | KeyCode::Esc => {
+                        app.input_mode = InputMode::Normal;
+                    }
+                    _ => {}
+                },
             }
         }
     }
