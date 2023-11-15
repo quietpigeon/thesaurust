4d2c76431ddba1def9b0bd0b450842e6e02d53e3
pigeon <6261rhys@gmail.com>
2023-11-16 07:31:26 +0900

    Rename help bar to controls and move it to the footer.

--- src/ui.rs
+++ src/ui.rs
@@ -1,50 +1,67 @@
 use ratatui::{
-    layout::{ Direction, Layout },
-    prelude::{ Alignment, Constraint },
-    style::{ Color, Modifier, Style, Stylize },
-    widgets::{ Block, Borders, List, ListItem, Paragraph, Wrap },
+    layout::{Direction, Layout},
+    prelude::{Alignment, Constraint},
+    style::{Color, Modifier, Style, Stylize},
+    widgets::{Block, Borders, List, ListItem, Paragraph, Wrap},
 };
 
-use crate::{ app::{ App, InputMode }, data::Thesaurus, tui::Frame, banner::BANNER};
+use crate::{
+    app::{App, InputMode},
+    banner::BANNER,
+    data::Thesaurus,
+    tui::Frame,
+};
 
 pub fn render(app: &mut App, f: &mut Frame) {
     // Main frame.
     let main_frame = Layout::default()
         .direction(Direction::Vertical)
-        .margin(1)
-        .constraints([Constraint::Length(3), Constraint::Length(9), Constraint::Min(1)].as_ref())
+        .margin(2)
+        .constraints(
+            [
+                Constraint::Length(3),
+                Constraint::Length(9),
+                Constraint::Length(3),
+                Constraint::Min(1),
+            ]
+            .as_ref(),
+        )
         .split(f.size());
 
     // The `upper_frame` consists of the search bar and the help bar.
     let upper_frame = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(70), Constraint::Percentage(30)].as_ref())
+        .constraints([Constraint::Percentage(100)].as_ref())
         .horizontal_margin(1)
         .split(main_frame[0]);
 
     // The `lower_frame` consists of the `part_of_speech` block and the `definitions` block.
     let lower_frame = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints(
-            [
-                Constraint::Percentage(20),
-                Constraint::Percentage(80),
-            ].as_ref()
-        )
+        // Part of speech (20%), right frame (80%)
+        .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
         .horizontal_margin(1)
         .split(main_frame[1]);
 
     // The frame for banner.
     let banner_frame = Layout::default()
-        .constraints([Constraint::Percentage(100), Constraint::Percentage(0)])
+        .constraints([Constraint::Percentage(100)])
         .margin(1)
         .split(main_frame[1]);
 
     // The `right_frame` consists of the `definition` block and the `example` block.
     let right_frame = Layout::default()
         .direction(Direction::Vertical)
+        // Definitions(50%), Examples(50%)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
         .split(lower_frame[1]);
+
+    // The `footer` consists of a spacer and `controls`.
+    let footer = Layout::default()
+        .direction(Direction::Vertical)
+        // Spacer(50%), Controls(50%)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+        .split(main_frame[2]);
 
     let mut definition = String::from("");
     let mut example = String::from("");
@@ -65,7 +82,9 @@ pub fn render(app: &mut App, f: &mut Frame) {
         // They are also used to show whether the word has multiple definitions or not.
         let meanings = app.results[0].meanings.clone();
         if meanings.is_some() {
-            let selections: Vec<ListItem> = app.selections.items
+            let selections: Vec<ListItem> = app
+                .selections
+                .items
                 .iter()
                 .map(|i| ListItem::new(i.content.clone()))
                 .collect();
@@ -74,13 +93,11 @@ pub fn render(app: &mut App, f: &mut Frame) {
                 .block(
                     Block::default()
                         .borders(Borders::ALL)
-                        .title( match app.input_mode {
-                            InputMode::Selecting => {
-                                "SELECT"
-                            }
-                            _ => {"Part of speech"}
+                        .title(match app.input_mode {
+                            InputMode::Selecting => "SELECT",
+                            _ => "Part of speech",
                         })
-                        .title_alignment(Alignment::Center)
+                        .title_alignment(Alignment::Center),
                 )
                 .style(Style::default().fg(Color::Green))
                 .highlight_style(Style::default().fg(Color::Black).bg(Color::Cyan));
@@ -95,7 +112,7 @@ pub fn render(app: &mut App, f: &mut Frame) {
                 .style(Style::default().fg(Color::Green))
                 .wrap(Wrap { trim: true })
                 .block(Block::default().borders(Borders::ALL).title("Definition")),
-            right_frame[0]
+            right_frame[0],
         );
 
         // Example block.
@@ -104,16 +121,14 @@ pub fn render(app: &mut App, f: &mut Frame) {
                 .style(Style::default().fg(Color::Green))
                 .wrap(Wrap { trim: true })
                 .block(Block::default().borders(Borders::ALL).title("Example")),
-            right_frame[1]
+            right_frame[1],
         );
     } else {
         f.render_widget(
-            Paragraph::new(
-                BANNER
-            )
+            Paragraph::new(BANNER)
                 .style(Style::default().fg(Color::Green))
                 .alignment(Alignment::Center),
-            banner_frame[0]
+            banner_frame[0],
         );
     }
     // Search bar.
@@ -125,15 +140,15 @@ pub fn render(app: &mut App, f: &mut Frame) {
             })
             .wrap(Wrap { trim: true })
             .block(Block::default().borders(Borders::ALL).title("Search")),
-        upper_frame[0]
+        upper_frame[0],
     );
 
-    // Help bar.
+    // Controls.
     f.render_widget(
-        Paragraph::new(String::from("Press `Esc` to stop running, `/` to start."))
+        Paragraph::new(String::from("<Esc>: Exit"))
             .style(Style::default().fg(Color::Green))
             .wrap(Wrap { trim: true })
-            .block(Block::default().borders(Borders::ALL).title("Help")),
-        upper_frame[1]
+            .block(Block::default().borders(Borders::NONE)),
+        footer[1],
     );
 }
