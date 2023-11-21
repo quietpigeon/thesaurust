d7bfc9ddbe987a3f82e6d87ca04114c79ace66f5
pigeon <91387291+QuietPigeon2001@users.noreply.github.com>
2023-11-22 00:56:42 +0900

    Create unit tests for methods used for updating stateful lists

--- src/app.rs
+++ src/app.rs
@@ -109,7 +109,7 @@ impl App {
 
 #[cfg(test)]
 mod tests {
-    use crate::data::Meaning;
+    use crate::data::{ Meaning, Definition };
 
     use super::*;
     use pretty_assertions::{ assert_eq };
@@ -120,17 +120,60 @@ mod tests {
         return mock_app;
     }
 
-    fn mock_results() -> Vec<Thesaurus> {
+    fn mock_part_of_speech() -> String {
+        String::from("noun")
+    }
+
+    fn mock_meaning_with(p: Option<String>, d: Option<Vec<Definition>>) -> Meaning {
+        Meaning { partOfSpeech: p, definitions: d }
+    }
+
+    fn mock_definition_with(d: Option<String>) -> Definition {
+        Definition { definition: d, example: None, synonyms: None, antonyms: None }
+    }
+
+    fn mock_results_with(m: Vec<Meaning>) -> Vec<Thesaurus> {
         vec![Thesaurus {
             word: Some(String::from("mock")),
             origin: None,
-            meanings: Some(
-                vec![Meaning {
-                    partOfSpeech: Some(String::from("noun")),
-                    definitions: None,
-                }]
-            ),
+            meanings: Some(m),
         }]
+    }
+
+    #[test]
+    fn test_update_part_of_speech_list() {
+        let mut mock_app = mock_app_in(InputMode::default());
+        let mock_parts_of_speech = vec![
+            String::from("noun"),
+            String::from("verb"),
+            String::from("adjective")
+        ];
+        let mock_meanings = mock_parts_of_speech
+            .clone()
+            .iter()
+            .map(|i| mock_meaning_with(Some(i.to_string()), None))
+            .collect();
+        mock_app.results = mock_results_with(mock_meanings);
+        App::update_stateful_lists(&mut mock_app, StatefulListType::PartOfSpeech);
+        assert_eq!(mock_parts_of_speech.len(), mock_app.part_of_speech_list.items.len());
+        assert_eq!(Some(0), mock_app.part_of_speech_list.state.selected())
+    }
+
+    #[test]
+    fn test_update_definition_list() {
+        let mut mock_app = mock_app_in(InputMode::default());
+        let mock_definitions = vec![
+            mock_definition_with(Some(String::from("Definition 1"))),
+            mock_definition_with(Some(String::from("Definition 2"))),
+            mock_definition_with(Some(String::from("Definition 3")))
+        ];
+        let mock_meanings = vec![
+            mock_meaning_with(Some(mock_part_of_speech()), Some(mock_definitions.clone()))
+        ];
+        mock_app.results = mock_results_with(mock_meanings);
+        App::update_stateful_lists(&mut mock_app, StatefulListType::All);
+        assert_eq!(mock_definitions.len(), mock_app.definition_list.items.len());
+        assert_eq!(Some(0), mock_app.definition_list.state.selected());
     }
 
     #[test]
@@ -142,18 +185,19 @@ mod tests {
     #[test]
     fn test_instructions_for_word_with_single_part_of_speech() {
         let mut mock_app = mock_app_in(InputMode::default());
-        let mock_results = mock_results();
-        mock_app.results = mock_results;
+        mock_app.results = mock_results_with(
+            vec![mock_meaning_with(Some(mock_part_of_speech()), None)]
+        );
         App::update_part_of_speech_list(&mut mock_app);
-        assert_eq!(1, mock_app.part_of_speech_list.items.len());
         assert_eq!(App::update_instructions(&mut mock_app), "l, h: Change definition; /: Insert");
     }
 
     #[test]
     fn test_instructions_in_normal_mode_with_results() {
         let mut mock_app = mock_app_in(InputMode::Normal);
-        let mock_results = mock_results();
-        mock_app.results = mock_results;
+        mock_app.results = mock_results_with(
+            vec![mock_meaning_with(Some(mock_part_of_speech()), None)]
+        );
         assert_eq!(true, !mock_app.results.is_empty());
         assert_eq!(
             App::update_instructions(&mut mock_app),
