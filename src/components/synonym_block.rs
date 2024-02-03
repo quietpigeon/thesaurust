34e70833db12130e088234ef67c990a0e9541f12
pigeon <6261rhys@gmail.com>
2024-02-03 20:23:04 +0900

    Update synonym block

--- src/components/synonym_block.rs
+++ src/components/synonym_block.rs
@@ -2,5 +2,15 @@ use ratatui::{ widgets::{ Block, Borders, List, ListItem }, style::{ Color, Styl
 
 use crate::models::app::{ App };
 
-fn new(app: &mut App) {
+fn new(app: &mut App) -> List {
+    let cloned_list = app.synonym_list.clone();
+    let synonyms: Vec<ListItem> = cloned_list.items
+        .iter()
+        .map(|i| ListItem::new(i.clone()))
+        .collect();
+    let synonyms = List::new(synonyms) 
+        .block(Block::default().borders(Borders::ALL).title("Synonyms"))
+        .style(Style::default().fg(Color::Green))
+        .highlight_style(Style::default().fg(Color::Black).bg(Color::Cyan));
+    synonyms
 }
